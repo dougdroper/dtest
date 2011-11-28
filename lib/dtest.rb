2bb203089676ee2b92782d1bf923b183b6de3b9a
Douglas Roper <douglasroper@notonthehighstreet.com>
2011-11-28 21:15:26 +0000

    Add backtrace line number

--- lib/dtest.rb
+++ lib/dtest.rb
@@ -5,6 +5,7 @@ module Dtest
   class Able
     def initialize(directory)
       @dtests_messages = []
+      @pending_messages = []
       @passed = 0
       @failed = 0
       @pending = 0
@@ -37,29 +38,34 @@ module Dtest
           rescue FailException => e
             print red "f"
             @failed += 1
-            @dtests_messages << [e.error_message + "\n#{klass}, \##{m} \n\n"]
+            @dtests_messages << [
+              e.error_message + 
+              "\n\n#{klass}, \##{m} \n" +
+              e.backtrace[1].to_s
+            ]
           rescue PendingExcepttion => e
             print yellow "P"
             @pending += 1
-            @dtests_messages << ["\n PENDING #{klass}, \##{m} \n\n"]
+            @pending_messages << ["\n PENDING #{klass}, \##{m}"]
           end
           @dtests_messages << assertion.dtests_messages
         end
       end
     end
 
-    def colorize(text, color_code)
+    def colour_dtest(text, color_code)
       "\e[#{color_code}m#{text}\e[0m"
     end
 
-    def red(text); colorize(text, 31); end
-    def green(text); colorize(text, 32); end
-    def yellow(text); colorize(text, 33); end
+    def red(text); colour_dtest(text, 31); end
+    def green(text); colour_dtest(text, 32); end
+    def yellow(text); colour_dtest(text, 33); end
 
     def print_dtests_results
       @dtests_messages.flatten!.each {|result| puts "\n" + red(result)}
+      @pending_messages.flatten!.each {|result| puts "\n" + yellow(result)}
 
-      puts "\n\n#{@passed + @failed + @pending} tests ran,  [e[32mDONEe[0m ] #{@passed} passed and #{@failed} failed"
+      puts "\n\n#{@passed + @failed + @pending} tests ran, #{@passed} passed and #{@failed} failed"
       puts "You have #{@pending} pending" if @pending > 0
     end
   end
